ca23c6dadb73fdc40692d4e7c29baa81dafbe083
Abhi <abhi@orbitwebtech.com>
2025-08-22 15:43:45 +0530

    structure change secound

--- Modules/Blog/resources/views/components/error.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['error'])
-@if ($error)
-    <ul {{ $attributes->merge(['class' => '','style'=>'color:red;']) }}>
-        @foreach ((array) $error as $item)
-            <p>{{ $item }}</p>
-        @endforeach
-    </ul>
-@endif
--- Modules/Blog/resources/views/livewire/posts/crud.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @if ($index_view === 1)
-    
-    @include('blog::livewire.posts.index')
-    @endif
-    @if ($add_edit_view === 1)
-    @include('blog::livewire.posts.add_edit')
-    @endif
-</div>
